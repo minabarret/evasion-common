b6ed635abf6db3c49a24184163a23e43f87b30ad
Oisin Mulvihill <oisin do-t mulvihill a-t gmail d-ot com>
2012-01-19 11:42:32 +0000

    refactoring out useful functionality into evasion common

---
+++ README.rst
@@ -0,0 +1,22 @@
+Evasion Common
+==============
+
+.. contents::
+
+EvasionProject code documentation
+---------------------------------
+
+  * http://www.evasionproject.com/apidocs/
+
+
+EvasionProject Wiki
+-------------------
+
+  * http://www.evasionproject.com/
+
+
+Introduction
+------------
+
+This package provides "evasion.common". This is a collection of useful
+functions and other utilities.
---
+++ runtests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Use nosetests to run the unit tests for this project.
+
+"""
+import sys
+import logging
+
+import nose
+
+log = logging.getLogger('evasion')
+hdlr = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+log.addHandler(hdlr)
+log.setLevel(logging.DEBUG)
+log.propagate = False
+
+sys.path.extend(["./lib",])
+
+# only run tests from here as the others it finds
+# are not unit tests.
+env = {}
+env['NOSE_WHERE'] = 'lib/evasion/common/tests,'
+
+result = nose.core.TestProgram(env=env).success
+nose.result.end_capture()
+
