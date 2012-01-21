34d23cd090ce73ecf4839e9f5a0ec1b5c06796ce
Oisin Mulvihill <oisin do-t mulvihill a-t gmail d-ot com>
2012-01-21 16:19:13 +0000

    Moved hub testing code off to messenger where is really belongs. Added skeletal failing tests for this module

---
+++ lib/evasion/common/tests/testevasioncommon.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import unittest
+
+
+class CommonTC(unittest.TestCase):
+
+
+    def testSignal(self):
+        """
+        """
+        self.assertEquals(1,0, "Not tested: evasion.common.signal")
+
+
+    def testNetTools(self):
+        """
+        """
+        self.assertEquals(1,0, "Not tested: evasion.common.net")
